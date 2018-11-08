ef5f2c55e4fbc0417c8a0d233d65fe344ccb9786
montes8 <emontesinos812@gmail.com>
2018-11-08 07:56:02 -0500

    modelo tabla usuario terminado

--- app/src/main/java/com/example/tayler_gabbi/monedero_java/Usuario.java
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/Usuario.java
@@ -3,6 +3,7 @@ package com.example.tayler_gabbi.monedero_java;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Generated;
 
 @Entity(nameInDb = "TBL_CLIENTE")
 public class Usuario {
@@ -18,4 +19,50 @@ public class Usuario {
     private String contrasenia;
     //@Transient -> permte obiar este campo a la hora d guardar a la bd
      //private String pais;varianle a eudir
+
+
+    @Generated(hash = 1324576134)
+    public Usuario(Long id, String nombre, String usuario, String contrasenia) {
+        this.id = id;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    @Generated(hash = 562950751)
+    public Usuario() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
