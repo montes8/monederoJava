7f94fc6289cc34238557f505f8893aa069496527
montes8 <emontesinos812@gmail.com>
2018-11-08 08:10:23 -0500

    configurando clase MonederoAplication

---
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/MonederoApplication.java
@@ -0,0 +1,24 @@
+package com.example.tayler_gabbi.monedero_java;
+
+import android.app.Application;
+
+import org.greenrobot.greendao.database.Database;
+
+public class MonederoApplication extends Application {
+
+    private DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this,"mydb");
+                Database db = helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
+}
