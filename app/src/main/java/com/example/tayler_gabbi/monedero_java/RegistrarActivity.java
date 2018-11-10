8fc5f734dd23b1a1fa5af4aaa1edb8a110e5f78b
montes8 <emontesinos812@gmail.com>
2018-11-09 21:42:43 -0500

    creando metodo listar usuario

--- app/src/main/java/com/example/tayler_gabbi/monedero_java/RegistrarActivity.java
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/RegistrarActivity.java
@@ -4,6 +4,8 @@ package com.example.tayler_gabbi.monedero_java;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class RegistrarActivity extends AppCompatActivity {
     private DaoSession daoSession;
 
@@ -27,5 +29,10 @@ public class RegistrarActivity extends AppCompatActivity {
     public void deleteUsuario(){
 
         daoSession.delete(1L);
+    }
+
+    public void listarUsuario(){
+
+        List<Usuario> usuarios = daoSession.getUsuarioDao().loadAll();
     }
 }
