da87ef674d01896e6c294343918ff3f314286415
montes8 <emontesinos812@gmail.com>
2018-11-13 21:49:49 -0500

    creando adapter de la lista usuarios

--- app/src/main/java/com/example/tayler_gabbi/monedero_java/Adapter/AdapterListaUsuario.java
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/Adapter/AdapterListaUsuario.java
@@ -1,4 +1,47 @@
 package com.example.tayler_gabbi.monedero_java.Adapter;
 
-public class AdapterListaUsuario  {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tayler_gabbi.monedero_java.model.Usuario;
+
+import java.util.ArrayList;
+
+
+public class AdapterListaUsuario extends RecyclerView.Adapter<AdapterListaUsuario.UsuarioViewholder> {
+
+
+    private ArrayList<Usuario> listaProductos;
+
+    public void addList(ArrayList<Usuario> listaProductos){
+        this.listaProductos = listaProductos;
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public UsuarioViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsuarioViewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class UsuarioViewholder extends RecyclerView.ViewHolder{
+        
+        public UsuarioViewholder(View itemView) {
+            super(itemView);
+
+        }
+    }
 }
