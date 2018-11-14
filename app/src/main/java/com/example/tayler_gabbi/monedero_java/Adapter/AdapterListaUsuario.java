3723501dc423c9317c7a31a450d196dc31444728
montes8 <emontesinos812@gmail.com>
2018-11-13 21:58:31 -0500

    creando UsuarioViewHolder

--- app/src/main/java/com/example/tayler_gabbi/monedero_java/Adapter/AdapterListaUsuario.java
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/Adapter/AdapterListaUsuario.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tayler_gabbi.monedero_java.R;
 import com.example.tayler_gabbi.monedero_java.model.Usuario;
 
 import java.util.ArrayList;
@@ -38,9 +39,17 @@ public class AdapterListaUsuario extends RecyclerView.Adapter<AdapterListaUsuari
     }
 
     public class UsuarioViewholder extends RecyclerView.ViewHolder{
-        
+
+
+        private String nombre,usuario,contrasenia;
+
         public UsuarioViewholder(View itemView) {
             super(itemView);
+
+            nombre = itemView.findViewById(R.id.text_view_nombre1);
+            usuario = itemView.findViewById(R.id.text_view_usuario1);
+            contrasenia = itemView.findViewById(R.id.text_view_password1);
+
 
         }
     }
