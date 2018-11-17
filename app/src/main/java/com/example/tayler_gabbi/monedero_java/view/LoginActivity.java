bfaff15f0abfa33973582fee6031ad0d8b7fc5d6
montes8 <emontesinos812@gmail.com>
2018-11-17 09:49:42 -0500

    corrigien do errores de validacion

--- app/src/main/java/com/example/tayler_gabbi/monedero_java/view/LoginActivity.java
+++ app/src/main/java/com/example/tayler_gabbi/monedero_java/view/LoginActivity.java
@@ -3,24 +3,22 @@ package com.example.tayler_gabbi.monedero_java.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.tayler_gabbi.monedero_java.MonederoApplication;
 import com.example.tayler_gabbi.monedero_java.R;
-import com.example.tayler_gabbi.monedero_java.model.DaoMaster;
-import com.example.tayler_gabbi.monedero_java.model.DaoSession;
 import com.example.tayler_gabbi.monedero_java.model.Usuario;
-import com.example.tayler_gabbi.monedero_java.listas.ListaUsuarioActivity;
 import com.example.tayler_gabbi.monedero_java.model.UsuarioDao;
 
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DaoMaster daoMaster;
-    private DaoSession daoSession;
-    private UsuarioDao loginDAO;
+    private UsuarioDao usuarioDao;
     private EditText txtUsuario;
     private EditText txtPassword;
     private Button logearse;
@@ -34,42 +32,38 @@ public class LoginActivity extends AppCompatActivity {
         txtUsuario = findViewById(R.id.edit_usuario);
         txtPassword = findViewById(R.id.edit_pasword);
         logearse = findViewById(R.id.button_ingresar);
+        usuarioDao = ((MonederoApplication)getApplication()).getDaoSession().getUsuarioDao();
 
-        logearse.setOnClickListener(new View.OnClickListener() {
+
+       logearse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                iniciarSesion();
-            }
-        });
+
+                String usuario = txtUsuario.getText().toString();
+                List listUsuario = usuarioDao.queryBuilder().where(UsuarioDao.Properties.Usuario.eq(usuario)).list();
 
 
-
-    }
-
-    //Comprobar si existe Usuario
-    public void iniciarSesion() {
-
-
-            String usuario = txtUsuario.getText().toString();
-            List<Usuario> listUsuario =loginDAO.queryBuilder().where(UsuarioDao.Properties.Usuario.eq(usuario)).list();
-                if(listUsuario!= null) {
-                    for(Usuario login : listUsuario) {
-
-                        if(usuario.equals(login.getUsuario())){
+                if(usuario.equals(""))
+                {
+                    Toast.makeText(LoginActivity.this, "Debe indicar el Usuario a comprobar.", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    if(listUsuario!= null) {
+                        for(Usuario usuario1 :listUsuario) {
                             Toast.makeText(this, "El usuario indicado existe en la Base de Datos." +
-                                    "\nID:" + login.getId() + "\nUsuario:" + login.getUsuario(),Toast.LENGTH_LONG).show();
-                        }else{
-                            Toast.makeText(this, "no existe usuarioEl usuario",Toast.LENGTH_LONG).show();
+                                    "\nID:" + login.getId() + "\nUsuario:" + login.getUsuario(),3000).show();
                         }
-
-
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "No hay coincidencias.", 3000).show();
                     }
                 }
-                else {
-                    Toast.makeText(this, "No hay coincidencias.", Toast.LENGTH_LONG).show();
-                }
             }
+       }
 
-
-
+            }
+        });
+     }
 }
